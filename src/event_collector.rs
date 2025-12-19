662cbfe9ff5b493888df78e6274a40d0fc55f4a1
Felipe Torres Gonzalez <torresfelipex1@gmail.com>
2025-12-19 16:03:21 +0100

    Increase even more the timeout for the tests

--- src/event_collector.rs
+++ src/event_collector.rs
@@ -372,7 +372,7 @@ mod tests {
         });
 
         // Give enough time to fetch the events.
-        sleep(Duration::from_secs(50)).await;
+        sleep(Duration::from_secs(60)).await;
 
         handle.abort();
 
