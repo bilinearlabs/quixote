0cd4dc946905eaff26990c835a66c42a534b6636
martinmsb <martinsb2010@gmail.com>
2026-05-12 11:51:12 +0200

    Add delay in test to avoid rate limit

--- src/event_collector.rs
+++ src/event_collector.rs
@@ -412,6 +412,10 @@ mod tests {
         provider_fixture: Arc<dyn Provider + Send + Sync + 'static>,
         mut seed_fixture: CollectorSeed,
     ) {
+        // Wait for check_transfer_events_for_a_block_range to complete first to avoid
+        // rate limiting interference between the two tests.
+        sleep(Duration::from_secs(25)).await;
+
         let (producer_buffer, mut consumer_buffer) = mpsc::channel(1000);
         let metrics = crate::metrics::MetricsHandle::default();
         // 10k throttles the RPC at the second request.
